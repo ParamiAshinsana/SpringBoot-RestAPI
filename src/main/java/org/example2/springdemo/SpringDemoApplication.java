e06e0b95655ce44ec4568432b337d1f9b3c9c7cc
Parami <paramiashinsana@gmail.com>
2024-08-12 20:56:00 +0530

    Update SpringDemoApplication

--- src/main/java/org/example2/springdemo/SpringDemoApplication.java
+++ src/main/java/org/example2/springdemo/SpringDemoApplication.java
@@ -1,12 +1,19 @@
 package org.example2.springdemo;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories
 public class SpringDemoApplication {
+
+	@Bean
+	ModelMapper modelMapper(){
+		return new ModelMapper();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringDemoApplication.class, args);
