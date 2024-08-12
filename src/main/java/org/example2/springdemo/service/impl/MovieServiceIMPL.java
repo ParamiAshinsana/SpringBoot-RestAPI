4cf6117375acceb70cdf9fb4e3efc4d39d42d067
Parami <paramiashinsana@gmail.com>
2024-08-12 20:50:03 +0530

    Update MovieServiceIMPL

--- src/main/java/org/example2/springdemo/service/impl/MovieServiceIMPL.java
+++ src/main/java/org/example2/springdemo/service/impl/MovieServiceIMPL.java
@@ -1,5 +1,12 @@
 package org.example2.springdemo.service.impl;
 
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
 public class MovieServiceIMPL {
 
 }
