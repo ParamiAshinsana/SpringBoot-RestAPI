6487a64a1273178ad038c77b1426e7fd9e50750b
Parami <paramiashinsana@gmail.com>
2024-08-12 20:49:43 +0530

    Update MovieMapping

--- src/main/java/org/example2/springdemo/mapping/MovieMapping.java
+++ src/main/java/org/example2/springdemo/mapping/MovieMapping.java
@@ -1,4 +1,9 @@
 package org.example2.springdemo.mapping;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
 public class MovieMapping {
 }
