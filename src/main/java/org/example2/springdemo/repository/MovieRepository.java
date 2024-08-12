6e6512208535d88edc0078ca3f4e924d179b89ea
Parami <paramiashinsana@gmail.com>
2024-08-12 21:15:57 +0530

    Update MovieRepository

--- src/main/java/org/example2/springdemo/repository/MovieRepository.java
+++ src/main/java/org/example2/springdemo/repository/MovieRepository.java
@@ -1,7 +1,9 @@
 package org.example2.springdemo.repository;
 
+import org.example2.springdemo.entity.MovieEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MovieRepository {
+public interface MovieRepository extends JpaRepository<MovieEntity, String> {
 }
