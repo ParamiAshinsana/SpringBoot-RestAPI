aedf08fccf7823f9777fc0246ca6da38be65b02f
Parami <paramiashinsana@gmail.com>
2024-08-12 21:09:22 +0530

    Update

--- src/main/java/org/example2/springdemo/service/MovieService.java
+++ src/main/java/org/example2/springdemo/service/MovieService.java
@@ -1,4 +1,13 @@
 package org.example2.springdemo.service;
 
+import org.example2.springdemo.dto.MovieDTO;
+
+import java.util.List;
+
 public interface MovieService {
+    MovieDTO saveMovie(MovieDTO movieDTO);
+    List<MovieDTO> getAllMovies();
+    MovieDTO getSelectedMovie(String id);
+    void deleteMovie(String id);
+    void updateMovie(String id, MovieDTO movieDTO);
 }
