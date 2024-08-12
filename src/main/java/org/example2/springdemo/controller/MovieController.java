f58082b40127a77c2d985681a62c25040906f7ea
Parami <paramiashinsana@gmail.com>
2024-08-12 20:20:10 +0530

    Update MovieController

--- src/main/java/org/example2/springdemo/controller/MovieController.java
+++ src/main/java/org/example2/springdemo/controller/MovieController.java
@@ -1,4 +1,13 @@
 package org.example2.springdemo.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin
+@RestController
+@RequestMapping("api/v1/movie")
+@RequiredArgsConstructor
 public class MovieController {
 }
