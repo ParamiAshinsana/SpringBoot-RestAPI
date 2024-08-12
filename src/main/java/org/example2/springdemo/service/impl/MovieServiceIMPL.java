6dd0b0b1241983c4f6e8cbf929d424ecda32dbe5
Parami <paramiashinsana@gmail.com>
2024-08-12 21:12:55 +0530

    Update MovieServiceIMPL

--- src/main/java/org/example2/springdemo/service/impl/MovieServiceIMPL.java
+++ src/main/java/org/example2/springdemo/service/impl/MovieServiceIMPL.java
@@ -2,11 +2,39 @@ package org.example2.springdemo.service.impl;
 
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.example2.springdemo.dto.MovieDTO;
+import org.example2.springdemo.service.MovieService;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @Transactional
 @RequiredArgsConstructor
-public class MovieServiceIMPL {
+public class MovieServiceIMPL implements MovieService {
 
+    @Override
+    public MovieDTO saveMovie(MovieDTO movieDTO) {
+        return null;
+    }
+
+    @Override
+    public List<MovieDTO> getAllMovies() {
+        return null;
+    }
+
+    @Override
+    public MovieDTO getSelectedMovie(String id) {
+        return null;
+    }
+
+    @Override
+    public void deleteMovie(String id) {
+
+    }
+
+    @Override
+    public void updateMovie(String id, MovieDTO movieDTO) {
+
+    }
 }
