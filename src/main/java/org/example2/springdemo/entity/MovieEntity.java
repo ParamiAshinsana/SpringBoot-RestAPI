e84b595ee05a409ebeca49b92a6ea05748bdc31a
Parami <paramiashinsana@gmail.com>
2024-08-12 20:49:33 +0530

    Update MovieEntity

--- src/main/java/org/example2/springdemo/entity/MovieEntity.java
+++ src/main/java/org/example2/springdemo/entity/MovieEntity.java
@@ -1,4 +1,17 @@
 package org.example2.springdemo.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "customer")
 public class MovieEntity {
 }
