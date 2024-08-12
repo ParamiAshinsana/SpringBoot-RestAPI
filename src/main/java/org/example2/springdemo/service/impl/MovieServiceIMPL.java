a58699048eb6198da3e88b8b0a17ca334a452af3
Parami <paramiashinsana@gmail.com>
2024-08-12 21:37:27 +0530

    Update MovieServiceIMPL

--- src/main/java/org/example2/springdemo/service/impl/MovieServiceIMPL.java
+++ src/main/java/org/example2/springdemo/service/impl/MovieServiceIMPL.java
@@ -3,6 +3,8 @@ package org.example2.springdemo.service.impl;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.example2.springdemo.dto.MovieDTO;
+import org.example2.springdemo.mapping.MovieMapping;
+import org.example2.springdemo.repository.MovieRepository;
 import org.example2.springdemo.service.MovieService;
 import org.springframework.stereotype.Service;
 
@@ -12,6 +14,8 @@ import java.util.List;
 @Transactional
 @RequiredArgsConstructor
 public class MovieServiceIMPL implements MovieService {
+    private final MovieRepository movieRepository;
+    private final MovieMapping movieMapping;
 
     @Override
     public MovieDTO saveMovie(MovieDTO movieDTO) {
